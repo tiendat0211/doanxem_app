92c7aa5abfcc4c11511597f4b29d5e7278505746
tiendat0211 <79643933+tiendat0211@users.noreply.github.com>
2022-10-13 19:13:23 +0700

    update

--- src/components/UserPostItem/UserPostItem.tsx
+++ src/components/UserPostItem/UserPostItem.tsx
@@ -12,9 +12,6 @@ import {
 import PressView from "../PressView/PressView";
 import FastImage from "react-native-fast-image";
 import { PostModel } from "../../model/ApiModel/PostModel";
-import AppColors from "../../styles/AppColors";
-import VideoPlayer from "react-native-video-player";
-import { IMG_NO_PICTURE } from "../../assets/path";
 
 interface UserPostItem {
   post: PostModel,
@@ -23,66 +20,18 @@ interface UserPostItem {
 
 
 const UserProfileItem: React.FC<UserPostItem> = (props) => {
-<<<<<<< HEAD
-  let { post } = props;
+  const { post,onPress} = props;
   const { colorPallet, theme } = useTheme()
 
-  function renderVideoOrImg(post: any) {
-    if (post.image!.endsWith('mp4') || post.type == 'video/mp4') {
-      return <VideoPlayer
-        customStyles={{
-          wrapper: {
-            backgroundColor:'red',
-            height: (Dimensions.get('screen').width - unit40 - unit32) / 3,
-            width: (Dimensions.get('screen').width - unit40 - unit32) / 3,
-            borderRadius: unit20
-          }
-        }
-        }
-        video={{ uri: post.image }}
-        videoWidth={1600}
-        videoHeight={900}
-        style={{
-          marginTop: unit12,
-          backgroundColor: AppColors.color_transparent_dark,
-        }}
-        showDuration={true}
-        defaultMuted={true}
-      // thumbnail={{ uri: IC_CLOSE }}
-      />;
-    } else {
-      <FastImage
-        source={{
-          uri: post?.image
-        }}
-        style={{
-          height: (Dimensions.get('screen').width - unit40 - unit32) / 3,
-          width: (Dimensions.get('screen').width - unit40 - unit32) / 3,
-          borderRadius: unit20
-        }}
-        resizeMode={FastImage.resizeMode.cover}
-      />
-    }
-  }
-
-=======
-  let { post,onPress} = props;
-  const { colorPallet, theme } = useTheme()
-
->>>>>>> #dat
   return (
     <>
       <PressView
-        onPress={() => {
-          console.log('post', post);
-        }}
         style={{
           marginRight: unit16,
           marginBottom: unit12
         }}
         onPress={onPress}
       >
-        {/* {renderVideoOrImg(post)} */}
         <FastImage
           source={{
             uri: post?.image.endsWith('mp4') ? 'https://www.techsmith.com/blog/wp-content/uploads/2019/06/YouTube-Thumbnail-Sizes.png' : post?.image
